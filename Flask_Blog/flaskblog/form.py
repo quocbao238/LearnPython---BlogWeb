2663520decdaa97a9ed776687bed518a63365e1a
Quốc Bảo <baobondev@gmail.com>
2020-12-25 20:23:02 +0700

    update

--- Flask_Blog/flaskblog/form.py
+++ Flask_Blog/flaskblog/form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models  import User
 class RegistrationForm(FlaskForm):
     username  = StringField('UserName',validators = [DataRequired(), Length(min=2, max = 20)])
@@ -13,6 +13,11 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username = username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
+        
+    def validate_email(self,email): 
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
     
 class LoginForm(FlaskForm):
